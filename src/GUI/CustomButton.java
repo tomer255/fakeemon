a747931d5b3705842adb9cc6e7f2c62dca8a5c1e
tomer255 <tomer255255@gmail.com>
2022-01-14 13:47:08 +0200

    change variable type from 'JButton' to 'CustomButton'

---
+++ src/GUI/CustomButton.java
@@ -0,0 +1,88 @@
+package GUI;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.*;
+import javax.swing.border.Border;
+
+
+public class CustomButton extends JButton implements MouseListener{
+    Font defaultFont = new Font("Gill Sans MT",Font.BOLD,14);
+    Color textColor = Color.decode("#ffffff");
+    Color backgroundColor = Color.decode("#00ff99");
+    Color hoverColor = Color.decode("#66ffc2");
+
+    public CustomButton(String s) {
+        this.setFocusPainted(false);
+        this.setText(s);
+        this.setBorder(new RoundedBorder(50));
+//        this.setBorder(null);
+        this.setForeground(textColor);
+        this.setBackground(backgroundColor);
+        this.setFont(defaultFont);
+        this.setOpaque(true);
+        addMouseListener(this);
+    }
+    public CustomButton(String s, Color backgroundColor, Color hoverColor) {
+        this.setFocusPainted(false);
+        this.setText(s);
+        this.setBorder(null);
+        this.setHoverColor(hoverColor);
+        this.setBackground(backgroundColor);
+        this.setFont(defaultFont);
+        this.setOpaque(true);
+        addMouseListener(this);
+    }
+
+    public void setBackgroundColor(Color color) {
+        backgroundColor = color;
+    }
+    public void setHoverColor(Color color) {
+        hoverColor = color;
+    }
+
+    @Override public void mouseClicked(MouseEvent me) {}
+    @Override public void mouseReleased(MouseEvent me) {}
+    @Override public void mousePressed(MouseEvent me) {}
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        if (e.getSource()==this) {
+            this.setBackground(this.hoverColor);
+        }
+    }
+    @Override
+    public void mouseExited(MouseEvent e) {
+        if (e.getSource()==this) {
+            this.setBackground(this.backgroundColor);
+        }
+    }
+
+    class RoundedBorder implements Border {
+
+        private int radius;
+
+
+        RoundedBorder(int radius) {
+            this.radius = radius;
+        }
+
+
+        public Insets getBorderInsets(Component c) {
+            return new Insets(this.radius+1, this.radius+1, this.radius+2, this.radius);
+        }
+
+
+        public boolean isBorderOpaque() {
+            return true;
+        }
+
+
+        public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+            g.drawRoundRect(x, y, width-1, height-1, radius, radius);
+        }
+    }
+
+}
+
