04731349f4cd01871b59c6bbcda70eea7237db27
tomer255 <tomer255255@gmail.com>
2022-01-21 00:31:49 +0200

    Create MoveListGenerator

---
+++ src/Fakeemon/MovesListGenerator.java
@@ -0,0 +1,133 @@
+package Fakeemon;
+
+import Moves.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MovesListGenerator {
+
+
+    public static List<Move> getMovesList(TypeBalance.ElementType fakeemonType, int amountOfAttacks) {
+        List<Move> ShuffeldMoves = new ArrayList<>();
+        List<eMoves> MovesEnumArrayList = getEnumMoveList(fakeemonType);
+        Collections.shuffle(MovesEnumArrayList);
+        for (int i = 0; i < amountOfAttacks; i++) {
+            ShuffeldMoves.add(getMove(MovesEnumArrayList.get(i)));
+        }
+        return ShuffeldMoves;
+    }
+
+    public static Move getMove(eMoves eMovesEnum){
+        Move move = null;
+        switch (eMovesEnum){
+            case BlazeKick -> move = new BlazeKick();
+            case FireSpin -> move = new FireSpin();
+            case FlameWheel -> move = new FlameWheel();
+            case HeatWave -> move = new HeatWave();
+            case OverHeat -> move = new OverHeat();
+            case Absorb -> move = new Absorb();
+            case BranchPoke -> move = new BranchPoke();
+            case GigaDrain -> move = new GigaDrain();
+            case RazorLeaf -> move = new RazorLeaf();
+            case SeedFlare -> move = new SeedFlare();
+            case BouncyBubble -> move = new BouncyBubble();
+            case Bubble -> move = new Bubble();
+            case BubbleBeam -> move = new BubbleBeam();
+            case SplishySplash -> move = new SplishySplash();
+            case WaterSpout -> move = new WaterSpout();
+            case Nuzzle -> move = new Nuzzle();
+            case Thunder -> move = new Thunder();
+            case ThunderPunch -> move = new ThunderPunch();
+            case ThunderShock -> move = new ThunderShock();
+            case VoltTackle -> move = new VoltTackle();
+            case Aeroblast -> move = new Aeroblast();
+            case AirCutter -> move = new AirCutter();
+            case FloatyFall -> move = new FloatyFall();
+            case Peck -> move = new Peck();
+            case SkyAttack -> move = new SkyAttack();
+        }
+        return move;
+    }
+
+    public static List<eMoves> getEnumMoveList(TypeBalance.ElementType elementType){
+        List<eMoves> eMovesList = null;
+        switch (elementType){
+            case Fire -> eMovesList = new ArrayList<>(){{
+                    add(eMoves.BlazeKick);
+                    add(eMoves.FireSpin);
+                    add(eMoves.FlameWheel);
+                    add(eMoves.HeatWave);
+                    add(eMoves.OverHeat);
+                }};
+
+            case Grass -> eMovesList = new ArrayList<>(){{
+                add(eMoves.Absorb);
+                add(eMoves.BranchPoke);
+                add(eMoves.GigaDrain);
+                add(eMoves.RazorLeaf);
+                add(eMoves.SeedFlare);
+            }};
+
+            case Water -> eMovesList = new ArrayList<>(){{
+                add(eMoves.BouncyBubble);
+                add(eMoves.Bubble);
+                add(eMoves.BubbleBeam);
+                add(eMoves.SplishySplash);
+                add(eMoves.WaterSpout);
+            }};
+
+            case Electric -> eMovesList = new ArrayList<>(){{
+                add(eMoves.Nuzzle);
+                add(eMoves.Thunder);
+                add(eMoves.ThunderPunch);
+                add(eMoves.ThunderShock);
+                add(eMoves.VoltTackle);
+            }};
+
+            case Flying -> eMovesList = new ArrayList<>(){{
+                add(eMoves.Aeroblast);
+                add(eMoves.AirCutter);
+                add(eMoves.FloatyFall);
+                add(eMoves.Peck);
+                add(eMoves.SkyAttack);
+            }};
+        }
+
+        return eMovesList;
+    }
+
+    public enum eMoves{
+        //Fire
+        BlazeKick,
+        FireSpin,
+        FlameWheel,
+        HeatWave,
+        OverHeat,
+        //Grass
+        Absorb,
+        BranchPoke,
+        GigaDrain,
+        RazorLeaf,
+        SeedFlare,
+        //Water
+        BouncyBubble,
+        Bubble,
+        BubbleBeam,
+        SplishySplash,
+        WaterSpout,
+        //Electric
+        Nuzzle,
+        Thunder,
+        ThunderPunch,
+        ThunderShock,
+        VoltTackle,
+        //Flying
+        Aeroblast,
+        AirCutter,
+        FloatyFall,
+        Peck,
+        SkyAttack
+    }
+}
