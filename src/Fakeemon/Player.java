32715d54e801431dae9c913c58b2b47eb9fd00b9
tomer255 <tomer255255@gmail.com>
2022-01-21 01:26:53 +0200

    Add items to player

--- src/Fakeemon/Player.java
+++ src/Fakeemon/Player.java
@@ -1,16 +1,44 @@
 package Fakeemon;
 
+import Item.Iitem;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-    private List<Fakeemon> Fakeemons = new ArrayList<>();
+
+
+    private final List<Fakeemon> fakeemons = new ArrayList<>();
+
+    private Fakeemon currentFakeemon = null;
+
+    private final List<Iitem> items = new ArrayList<>();
+
+    public List<Iitem> getItems() {
+        return items;
+    }
+
+    public List<Fakeemon> getFakeemons()
+    {
+        return fakeemons;
+    }
 
     public void addFakeemon(Fakeemon fakeemon){
-        Fakeemons.add(fakeemon);
+        if(currentFakeemon == null){
+            currentFakeemon = fakeemon;
+        }
+        fakeemons.add(fakeemon);
+    }
+
+    public void setCurrentFakeemon(Fakeemon currentFakeemon) {
+        this.currentFakeemon = currentFakeemon;
+    }
+
+    public void addItem(Iitem item){
+        items.add(item);
     }
 
     public Fakeemon getFakeemon(){
-        return Fakeemons.get(0);
+        return currentFakeemon;
     }
 }
