b1ccaadca07c7934adc2040bcbf18e8c775c71e3
tomer255 <tomer255255@gmail.com>
2022-01-20 22:25:34 +0200

    Add Flying and Electric to ElementType in TypeBalance.

--- src/Fakeemon/TypeBalance.java
+++ src/Fakeemon/TypeBalance.java
@@ -6,32 +6,51 @@ public class TypeBalance {
 
     static TypeBalance typeBalance = new TypeBalance();
 
-    private float[][] bonus = new float[Type.values().length][Type.values().length];
+    private final float[][] bonus = new float[ElementType.values().length][ElementType.values().length];
 
     private TypeBalance(){
 
-        setBonus(Type.Fire,Type.Fire,0.5f);
-        setBonus(Type.Fire,Type.Water,0.5f);
-        setBonus(Type.Fire,Type.Grass,2.0f);
+        setBonus(ElementType.Fire, ElementType.Fire,0.5f);
+        setBonus(ElementType.Fire, ElementType.Water,0.5f);
+        setBonus(ElementType.Fire, ElementType.Grass,2.0f);
+        setBonus(ElementType.Fire, ElementType.Flying,1.0f);
+        setBonus(ElementType.Fire, ElementType.Electric,1.0f);
 
-        setBonus(Type.Water,Type.Fire,2.0f);
-        setBonus(Type.Water,Type.Water,0.5f);
-        setBonus(Type.Water,Type.Grass,0.5f);
+        setBonus(ElementType.Water, ElementType.Fire,2.0f);
+        setBonus(ElementType.Water, ElementType.Water,0.5f);
+        setBonus(ElementType.Water, ElementType.Grass,0.5f);
+        setBonus(ElementType.Water, ElementType.Flying,1.0f);
+        setBonus(ElementType.Water, ElementType.Electric,1.0f);
 
-        setBonus(Type.Grass,Type.Fire,0.5f);
-        setBonus(Type.Grass,Type.Water,2.0f);
-        setBonus(Type.Grass,Type.Grass,0.5f);
+        setBonus(ElementType.Grass, ElementType.Fire,0.5f);
+        setBonus(ElementType.Grass, ElementType.Water,2.0f);
+        setBonus(ElementType.Grass, ElementType.Grass,0.5f);
+        setBonus(ElementType.Grass, ElementType.Flying,0.5f);
+        setBonus(ElementType.Grass, ElementType.Electric,1.0f);
+
+        setBonus(ElementType.Flying, ElementType.Fire,1.0f);
+        setBonus(ElementType.Flying, ElementType.Water,1.0f);
+        setBonus(ElementType.Flying, ElementType.Grass,2.0f);
+        setBonus(ElementType.Flying, ElementType.Flying,1.0f);
+        setBonus(ElementType.Flying, ElementType.Electric,0.5f);
+
+        setBonus(ElementType.Electric, ElementType.Fire,1.0f);
+        setBonus(ElementType.Electric, ElementType.Water,2.0f);
+        setBonus(ElementType.Electric, ElementType.Grass,0.5f);
+        setBonus(ElementType.Electric, ElementType.Flying,2.0f);
+        setBonus(ElementType.Electric, ElementType.Electric,0.5f);
+
     }
 
-    private void setBonus(Type attacker,Type defender,float vale){
-        bonus[attacker.ordinal()][defender.ordinal()] = vale;
+    private void setBonus(ElementType attacker, ElementType defender, float value){
+        bonus[attacker.ordinal()][defender.ordinal()] = value;
     }
 
-    public static float getBonus(Type attacker,Type defender){
+    public static float getBonus(ElementType attacker, ElementType defender){
         return typeBalance.bonus[attacker.ordinal()][defender.ordinal()];
     }
 
-    public enum Type{
+    public enum ElementType {
         Fire,
         Water,
         Grass,
