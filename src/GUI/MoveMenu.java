c54419128f03ebb242ec489d76711c90daa01871
tomer255 <tomer255255@gmail.com>
2022-01-20 18:32:10 +0200

    Complete move menu

--- src/GUI/MoveMenu.java
+++ src/GUI/MoveMenu.java
@@ -1,37 +1,34 @@
 package GUI;
 
-import Fakeemon.Fakeemon;
+import Fakeemon.Player;
 import Moves.Move;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MoveMenu extends JPanel {
-
-//    private CustomButton btnA;
-//    private CustomButton btnB;
-//    private CustomButton btnC;
-//    private CustomButton btnD;
 
     MoveMenu(){
         this.setLayout(new GridLayout(2,2,15,15));
         this.setBackground(new Color(1.0f,1.0f,1.0f,0.0f));
-
-//        btnA = new CustomButton("A");
-//        btnB = new CustomButton("B");
-//        btnC = new CustomButton("C");
-//        btnD = new CustomButton("D");
-//
-//        this.add(btnA);
-//        this.add(btnB);
-//        this.add(btnC);
-//        this.add(btnD);
     }
 
-//    void setFakeemon(Fakeemon fakeemon){
-//        for (Move move: fakeemon.getAttackPool()) {
-//            CustomButton btn = new CustomButton(move.getName());
-//
-//        }
-//    }
+    public void setButtonsPanel(BattleFrame battleFrame){
+        this.removeAll();
+        Player player1 = battleFrame.getCurrPlayer();
+        Player player2 = battleFrame.getEnemyPlayer();
+        for (Move move: player1.getFakeemon().getAttackPool()) {
+            CustomButton btnMove = new CustomButton(move.getName());
+            btnMove.addActionListener((event) -> {
+                    move.use(player1.getFakeemon(),player2.getFakeemon());
+                    btnMove.setText(move.getName());
+                    battleFrame.showFeedback();
+                }
+            );
+            btnMove.setToolTipText(move.getDescription());
+            this.add(btnMove);
+        }
+    }
 }
