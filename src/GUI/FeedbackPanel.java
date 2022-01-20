7d2a102117f8548f425f8afb456fc7d7c9c5f2ab
tomer255 <tomer255255@gmail.com>
2022-01-21 01:32:31 +0200

    Add FeedbackPanel

---
+++ src/GUI/FeedbackPanel.java
@@ -0,0 +1,37 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FeedbackPanel extends JPanel {
+
+    String htmlContent = "<html><p style=\"text-align:center\"><strong><span style=\"font-size:24px\"><span style=\"font-family:Comic Sans MS,cursive\">test</span></span></strong>\uD83D\uDE00</p></html>";
+
+    private final JLabel feedbackLabel = new JLabel(htmlContent);
+    private final CustomButton btnOk = new CustomButton("OK");
+
+
+    public FeedbackPanel(BattleFrame battleFrame) {
+        btnOk.addActionListener((event) -> battleFrame.endTurn()
+        );
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.setBackground(new Color(1.0f, 1.0f, 1.0f, 0.0f));
+
+        feedbackLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        btnOk.setAlignmentX(CustomButton.CENTER_ALIGNMENT);
+        btnOk.setPreferredSize(new Dimension(20, 20));
+
+        this.add(feedbackLabel);
+        this.add(btnOk);
+    }
+
+    public void setLabel(String msg) {
+        feedbackLabel.setText(msg);
+    }
+
+    public JButton getBtnOk() {
+        return btnOk;
+    }
+
+
+}
