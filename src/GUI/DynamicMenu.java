0f2cbbed18817246131d51f9613b4798872ee038
tomer255 <tomer255255@gmail.com>
2022-01-21 01:38:29 +0200

    Move menus from local variables to members

--- src/GUI/DynamicMenu.java
+++ src/GUI/DynamicMenu.java
@@ -4,16 +4,38 @@ import javax.swing.*;
 import java.awt.*;
 
 public class DynamicMenu extends JPanel {
-    MoveMenu moveMenu = new MoveMenu();
+    MoveMenu moveMenu;
+    ItemMenu itemMenu;
+    FeedbackPanel feedbackPanel;
+    ChangeFakeemonMenu cahngeFakeemonMenu;
 
-    DynamicMenu(){
+
+    DynamicMenu(BattleFrame battleFrame){
         this.setBounds(50,200,300,150);
         this.setBackground(new Color(1.0f,1.0f,1.0f,0.0f));
 //        this.setLayout(null);// TODO: set Layout
-        this.setLayout(new GridLayout(1,1,0,0));
+        this.setLayout(new BorderLayout());
+        moveMenu = new MoveMenu();
+        itemMenu = new ItemMenu();
+        feedbackPanel = new FeedbackPanel(battleFrame);
+        cahngeFakeemonMenu = new ChangeFakeemonMenu();
     }
 
     public MoveMenu getMoveMenu() {
         return moveMenu;
+    }
+
+    public ItemMenu getItemMenu() {
+        return itemMenu;
+    }
+
+    public FeedbackPanel getFeedbackPanel()
+    {
+        return feedbackPanel;
+    }
+
+    public ChangeFakeemonMenu getCahngeFakeemonMenu()
+    {
+        return cahngeFakeemonMenu;
     }
 }
