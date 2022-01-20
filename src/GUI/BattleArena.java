ec1da4f982d53de431db479696e681af4fa4b9ac
tomer255 <tomer255255@gmail.com>
2022-01-21 01:36:50 +0200

    Change receive type for 'updateDisplay' method

--- src/GUI/BattleArena.java
+++ src/GUI/BattleArena.java
@@ -2,26 +2,27 @@ package GUI;
 
 import javax.swing.*;
 import java.awt.*;
-import Fakeemon.Fakeemon;
 
 public class BattleArena extends JPanel {
 
     FakeemonDisplay fakeemonDisplay1;
     FakeemonDisplay fakeemonDisplay2;
 
-    BattleArena(Fakeemon firstFakeemon,Fakeemon secondFakeemon){
+    BattleArena(BattleFrame battleFrame){
         this.setLayout(new GridLayout(2,1,15,15));
         this.setBounds(50,20,300,160);
         this.setBackground(new Color(1.0f,1.0f,1.0f,0.0f));
-        fakeemonDisplay1 = new FakeemonDisplay(FakeemonDisplay.playerLocation.first,firstFakeemon);
-        fakeemonDisplay2 = new FakeemonDisplay(FakeemonDisplay.playerLocation.second,secondFakeemon);
+        fakeemonDisplay2 = new FakeemonDisplay(FakeemonDisplay.playerLocation.first,battleFrame.getEnemyPlayer());
+        fakeemonDisplay1 = new FakeemonDisplay(FakeemonDisplay.playerLocation.second,battleFrame.getCurrPlayer());
 
-        this.add(fakeemonDisplay1);
         this.add(fakeemonDisplay2);
+        this.add(fakeemonDisplay1);
     }
 
-    public void update(){
-        fakeemonDisplay1 .update();
-        fakeemonDisplay2.update();
+    public void updateDisplay(BattleFrame battleFrame){
+        fakeemonDisplay1.updateDisplay(battleFrame.getCurrPlayer());
+        fakeemonDisplay2.updateDisplay(battleFrame.getEnemyPlayer());
+        battleFrame.repaint();
+        battleFrame.revalidate();
     }
 }
