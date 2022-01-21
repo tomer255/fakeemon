753e80d2ac9657454b2276774f67c1a76fd7635b
Idan Horovitz <idanhh55@gmail.com>
2022-01-21 23:56:40 +0200

    Added feedback message

--- src/Moves/Move.java
+++ src/Moves/Move.java
@@ -1,4 +1,5 @@
 package Moves;
+
 import Fakeemon.*;
 
 import java.util.Random;
@@ -15,25 +16,39 @@ public abstract class Move {
         return name + " " + PP + '/' + MaxPP;
     }
 
-    public void use(Fakeemon attacker, Fakeemon defender){
+    public String use(Fakeemon attacker, Fakeemon defender) {
         Random rand = new Random();
-        if(PP <= 0){
-            System.out.println("No PP left");
-            return;
-        }
-        if(getFinalAccuracy(defender) < rand.nextFloat()){
-            System.out.println("Miss");
-        }
-        else {
+        String returnMassage;
+        if (PP <= 0) {
+            return  "No PP left";
+        } else if (getFinalAccuracy(defender) < rand.nextFloat()) {
+            returnMassage = "Miss";
+        } else {
             activate(attacker, defender);
+            returnMassage = getActivateMassage(defender);
         }
         PP--;
+        return returnMassage;
     }
 
-    private float getFinalAccuracy(Fakeemon fakeemon){
-        return accuracy - fakeemon.getSpeed()*0.05f;
+    String getActivateMassage(Fakeemon defender) {
+        float effectiveness = TypeBalance.getBonus(type, defender.getType());
+        if(effectiveness < 1){
+            return "Move was not very effective";
+        }
+        else if (effectiveness > 1){
+            return "Move was very effective";
+        }
+        else{
+            return "Move was effective";
+        }
+    }
+
+    private float getFinalAccuracy(Fakeemon fakeemon) {
+        return accuracy - fakeemon.getSpeed() * 0.05f;
     }
 
     abstract void activate(Fakeemon attacker, Fakeemon defender);
+
     public abstract String getDescription();
 }
