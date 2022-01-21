aa930b00da7de4905cdc06c695e78d615a231b75
Idan Horovitz <idanhh55@gmail.com>
2022-01-21 22:45:33 +0200

    Create unit tests

--- src/Test/Moves/AbsorbTest.java
+++ src/Test/Moves/AbsorbTest.java
@@ -1,10 +1,8 @@
 package Moves;
 import Fakeemon.Fakeemon;
-import Fakeemon.MovesListGenerator;
 import Fakeemon.TypeBalance;
 import org.junit.Test;
 
-import junit.framework.TestCase;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,22 +12,21 @@ import static junit.framework.TestCase.assertEquals;
 public class AbsorbTest {
 
     @Test
-    public void testActivate() {
-        int amountOfAttacks = 4;
+    public void testActivateAbsorbGrassVsFire(){
 
         List<Move> moves = new ArrayList<>();
         Fakeemon fakeemonAttacker = new Fakeemon("bamboon", TypeBalance.ElementType.Grass, 300, 250,
                 moves, 3.4f, 3.6f, 3.8f, 3f, 4f);
 
         Fakeemon fakeemonDefender = new Fakeemon("agnite", TypeBalance.ElementType.Fire, 260, 260,
-                moves, 3.7f, 3.4f, 3.8f, 3f, 5f);
-
+                moves, 3.7f, 2f, 3.8f, 3f, 5f);
         Move testMove = new Absorb();
+
+        //Test number 1:
         testMove.activate(fakeemonAttacker, fakeemonDefender);
-//        assertEquals(320f, fakeemon.getCurrentHP());
-
-
-
+        assertEquals(255f, fakeemonDefender.getCurrentHP());
+        assertEquals(255f, fakeemonAttacker.getCurrentHP());
 
     }
+
 }
